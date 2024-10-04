4f7697a456db9fe34053880691e50b7dc6a922f2
JoraAv3 <jklu42@gmail.com>
2024-10-04 17:47:17 +0400

    Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {host: true},
   preview: {
-    port: 5173,
+    port: 5173,    
   },
 });
