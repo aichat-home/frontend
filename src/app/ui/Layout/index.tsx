dbda1097bd4b2a36eb912689c135cc1b2b1db627
JoraAv3 <jklu42@gmail.com>
2024-10-05 22:40:23 +0400

    Ref url

--- src/app/ui/Layout/index.tsx
+++ src/app/ui/Layout/index.tsx
@@ -1,26 +1,8 @@
 import {  Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import "./index.css";
 import Footer from "../../../widgets/ui/Footer";
-import { useEffect } from "react";
-import { useBackButton } from "@telegram-apps/sdk-react";
 
 const Layout = () => {
-  const backBtn = useBackButton()
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    console.log("location", location)
-    if (location.pathname !== "/") {
-      backBtn.on("click", () => {
-        navigate('/')
-      })
-      backBtn.show()
-    } else {
-      backBtn.hide()
-      backBtn.off("click", () => {})
-    }
-  }, [location])
   return (
     <div className="layout-container">
       <main className="content">
